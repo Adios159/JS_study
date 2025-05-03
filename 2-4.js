80c57572e89653cba7e0bb33739399410044a800
Adios159 <83947563+Adios159@users.noreply.github.com>
2025-05-03 15:33:20 +0000

    자료형

---
+++ 2-4.js
@@ -0,0 +1,12 @@
+string = '문자열';
+string2 = "문자열";
+string3 = `문자열`
+
+string4 = "문자열 속의'작은 따옴표'는 문자열로 인식한다"
+string5 = "큰따옴표 안에 있는 '작은 따옴표'는 문자열로 인식한다"
+
+console.log(string)
+console.log(string2)
+console.log(string3)
+console.log(string4)
+console.log(string5)
