b321d705bac60853441309c1444a5a931794a942
Adios159 <83947563+Adios159@users.noreply.github.com>
2025-05-04 13:11:12 +0000

    연산자

---
+++ 3-1.js
@@ -0,0 +1,18 @@
+var oper1 = 5 + 2
+var oper2 = 5 - 2
+var oper3 = 5 * 2
+var oper4 = 5 / 2
+var oper5 = 5 % 2
+
+var qwer = 1
+var oper6 = qwer++
+var oper7 = qwer--
+
+
+console.log(oper1)
+console.log(oper2)
+console.log(oper3)
+console.log(oper4)
+console.log(oper5)
+console.log(oper6)
+console.log(oper7)
